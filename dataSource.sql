af4c20a6604fef8d6f6f042eb25ba2e2e22c54d5
hayki <Hayk.inanc@gmail.com>
2019-10-27 01:24:22 +0300

    init commit2

--- dataSource.sql
+++ dataSource.sql
@@ -13,12 +13,12 @@ create table dataSource (
 );
 
 insert into dataSource (first_name, last_name, email, gender) 
-    values  
-    (null,                      'Hilda Sodo',           'hsodo1o@surveymonkey.com',                       'F'),                        
-    ('Torin Cardus',             null,                  'tcardus21@ow.ly',                                'Male'),                        
-    (null,                      'Artur MacShane',       'amacshane2d@princeton.edu',                      'M'),                        
-    ('Freedman Krause',          null,                  'fkrause5t@dagondesign.com',                      'Male'),                        
-    (null,                      'Lemmers Remington',    'rlemmers70@tripod.com',                          'Male'),                        
+	values  
+	(null,                      'Hilda Sodo',           'hsodo1o@surveymonkey.com',                       'F'),                        
+	('Torin Cardus',             null,                  'tcardus21@ow.ly',                                'Male'),                        
+	(null,                      'Artur MacShane',       'amacshane2d@princeton.edu',                      'M'),                        
+	('Freedman Krause',          null,                  'fkrause5t@dagondesign.com',                      'Male'),                        
+	(null,                      'Lemmers Remington',    'rlemmers70@tripod.com',                          'Male'),                        
 	('Tucker',                  'Crauford',             'tcraufords@chicagotribune.com +7 9346553 221',   'M'),                        
 	('Winfield',                'Sharpe',               'wsharpe5k@amazon.co.jp +7-912-321-84-43',        'Male'),                        
 	('Caresa',                  'Symmers',              '+79824433556',                                   'F'),     
@@ -27,7 +27,7 @@ insert into dataSource (first_name, last_name, email, gender)
 	('Paxon',                   'Rimington',            '89094235643',                   				  'Male'),                        
 	('Truda',                   'Biffin',               'tbiffin89@wired.com',                            'F'),                        
 	('Noland',                  'Buesden',              '893265432 85',                                   'Male'),                                           
-    ('Brana Champion',           null,                  'bchampiondv@csmonitor.com',                      'Female');
+	('Brana Champion',           null,                  'bchampiondv@csmonitor.com',                      'Female');
 
 
 
@@ -48,10 +48,10 @@ create table dataSource2 (
 
 
 insert into personsData2 (first_name, last_name, email, gender) 
-    values  
+	values  
 	('Rosita',                  'McGing',               'rmcging5@nps.gov 89235428443',                   'Female'),                        
 	('Elinor',                  'Barca',                'ebarca54@ning.com 89022338843',                  'Female'),                        
 	('Paxon',                   'Rimington',            '89094235643',                   				  'Male'),                        
 	('Truda',                   'Biffin',               'tbiffin89@wired.com',                            'F'),                        
 	('Noland',                  'Buesden',              '893265432 85',                                   'Male'),                                           
-    ('Brana Champion',           null,                  'bchampiondv@csmonitor.com',                      'Female');
+	('Brana Champion',           null,                  'bchampiondv@csmonitor.com',                      'Female');
