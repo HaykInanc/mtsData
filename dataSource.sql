ffbf03f7e91790873c3e22061d303ca441fbb775
hayk.inanc@gmail.com <hayk.inanc@gmail.com>
2019-12-24 11:25:26 +0300

    Update dataSource.sql

--- dataSource.sql
+++ dataSource.sql
@@ -64,6 +64,10 @@ insert into dataSource2 (first_name, last_name, email, gender)
 /*****************/
 
 
+
+drop table if exists personsData;
+
+ /* first_name, last_name, email, phone, gender */
 drop table if exists personsData;
 create table personsData (
 	id integer primary key autoincrement,
@@ -76,6 +80,31 @@ create table personsData (
 );
 
 
+drop table if exists dataSource;
+create table dataSource (
+	id integer primary key autoincrement,
+	first_name varchar(255),
+	last_name varchar(255),
+	email varchar(255),
+	gender varchar(255)
+);
+
+insert into dataSource (first_name, last_name, email, gender) 
+	values  
+	(null,                      'Hilda Sodo',           'hsodo1o@surveymonkey.com',                       'F'),                        
+	('Torin Cardus',             null,                  'tcardus21@ow.ly',                                'Male'),                        
+	(null,                      'Artur MacShane',       'amacshane2d@princeton.edu',                      'M'),                        
+	('Freedman Krause',          null,                  'fkrause5t@dagondesign.com',                      'Male'),                        
+	(null,                      'Lemmers Remington',    'rlemmers70@tripod.com',                          'Male'),                        
+	('Tucker',                  'Crauford',             'tcraufords@chicagotribune.com +7 9346553 221',   'M'),                        
+	('Winfield',                'Sharpe',               'wsharpe5k@amazon.co.jp +7-912-321-84-43',        'Male'),                        
+	('Caresa',                  'Symmers',              '+79824433556',                                   'F'),     
+	('Rosita',                  'McGing',               'rmcging5@nps.gov 89235428443',                   'Female'),                        
+	('Elinor',                  'Barca',                'ebarca54@ning.com 89022338843',                  'Female'),                        
+	('Paxon',                   'Rimington',            '89094235643',                   				  'Male'),                        
+	('Truda',                   'Biffin',               'tbiffin89@wired.com',                            'F'),                        
+	('Noland',                  'Buesden',              '893265432 85',                                   'Male'),                                           
+	('Brana Champion',           null,                  'bchampiondv@csmonitor.com',                      'Female');
 
 drop table if exists dataSource01;
 create table dataSource01 as 
@@ -116,3 +145,124 @@ case
 	else last_name
 end as last_name
 from dataSource;
+
+insert into personsData (first_name, last_name, email, phone, gender) select first_name, last_name, email, phone, gender from dataSource01;
+/*select * from personsData;*/
+
+
+/**********************/
+/**********************/
+/**********************/
+/**********************/
+/*      классная работа*/
+/**********************/
+
+
+drop table if exists dataSource2;
+
+
+create table dataSource2 (
+	id integer primary key autoincrement,
+	first_name varchar(255),
+	last_name varchar(255),
+	email varchar(255),
+	gender varchar(255)
+);
+
+
+insert into dataSource2 (first_name, last_name, email, gender) 
+	values  
+	('Rosita',                  'McGing',               'rmcging5@nps.gov 89235428443',                   'Female'),                        
+	('Elinor',                  'Barca',                'ebarca54@gmail.com 89022338843',                  'Female'),                        
+	('Paxon',                   'Rimington',            '89094235643',                   				  'Male'),                        
+	('Truda',                   'Biffin',               'tbiffin81@wired.com 89096450730',                            'F'),                        
+	('Noland',                  'Buesden',              '893265432 56',                                   'Male'),                                           
+	('Brana Champion',           null,                  'CBrana@csmonitor.com',                      'Female');
+
+
+drop table if exists dataSource02;
+create table dataSource02 as 
+select 
+case 
+	when instr(lower(gender), 'f') = 0
+		then 'male'
+		else 'female'
+end as gender,
+
+  case
+	  when instr(email, '@') = 0 then null
+	  when instr(email, ' ') = 0 then email
+	  when instr(email, ' ') <> 0 
+	  	then substr(email, 0 , instr(email, ' '))
+  end as email,
+
+  case 
+  	when instr(email, '@') = 0 then replace(email, ' ', '')
+	when instr(email, ' ') = 0 then null
+	when instr(email, ' ') <> 0
+		then replace(substr(email, instr(email, ' ')), ' ', '')
+  end as phone,
+
+  case
+	when instr(first_name, ' ')
+		then substr(first_name, 0, instr(first_name, ' '))
+	when first_name is null
+		then substr(last_name, 0, instr(last_name, ' '))
+	else first_name
+end as first_name,
+
+case
+	when instr(last_name, ' ')
+		then substr(last_name, instr(last_name, ' ')+1)
+	when last_name is null
+		then substr(first_name, instr(first_name, ' ')+1)
+	else last_name
+end as last_name
+from dataSource2;
+
+insert into personsData (first_name, last_name, email, phone, gender) select first_name, last_name, email, phone, gender from dataSource02;
+
+select * from personsData;
+
+
+/*
+	взять актуальный срез
+*/
+
+select 
+a.first_name,
+a.last_name,
+a.email,
+a.phone,
+a.gender,
+a.created_at
+from personsData a
+inner join (
+  select 
+  first_name, 
+  last_name,
+  max(created_at) as maxCreated_at
+  from personsData 
+  group by first_name, last_name
+) b
+  on a.first_name = b.first_name
+  and a.last_name = b.last_name
+  and a.Created_at = b.maxCreated_at
+
+
+/************/
+/************/
+/************/
+/************/
+/************/
+
+
+drop view if exists personCom;
+create view personCom as 
+select 
+first_name,
+last_name,
+created_at,
+datetime(created_at, 'start of month') as communication_from,
+datetime(created_at, 'start of month', '+3 month', '-1 second') as communication_to
+from v_personsData;
